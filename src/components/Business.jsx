d6e4127840cb987984dc249bd36f6457f76e1752
Fahid Javid <fahidjavid@gmail.com>
2025-05-10 12:04:51 +0500

    Added basic components for the whole page.

---
+++ src/components/Business.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Business = () => {
+    return (
+        <div>Business</div>
+    )
+}
+export default Business
---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CardDeal = () => {
+    return (
+        <div>CardDeal</div>
+    )
+}
+export default CardDeal
