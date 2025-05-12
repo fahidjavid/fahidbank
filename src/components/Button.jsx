66cd65236349ea9ab526f74a68d5c639bd7abe07
Fahid Javid <fahidjavid@gmail.com>
2025-05-12 23:51:36 +0500

    Applied the cursor to the button component.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ( { styles } ) => {
     return (
-        <button className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-md ${styles}`} type="button">
+        <button className={`py-4 px-6 bg-blue-gradient cursor-pointer font-poppins font-medium text-[18px] text-primary outline-none rounded-md ${styles}`} type="button">
             Get Started </button>
     )
 }
