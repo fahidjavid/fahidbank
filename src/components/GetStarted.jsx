32711e5924f2443b946474d0f6c60f3e5720b10a
Fahid Javid <fahidjavid@gmail.com>
2025-05-14 21:41:40 +0500

    Improved get started layout.

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,7 +3,7 @@ import { arrowUp } from "../assets"
 
 const GetStarted = () => {
     return (
-        <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+        <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] hover:p-[4px] cursor-pointer transition-all duration-200 ease-in-out`}>
             <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
                 <div className={`${styles.flexStart} flex-row`}>
                     <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
